eb31c4a15958e871a0186bb688450f8dd1757f70
Matt Brictson <matt@55minutes.com>
2014-05-31 13:50:16 -0700

    Switch from Term::ANSIColor to colorize

--- lib/capistrano/fiftyfive/compatibility.rb
+++ lib/capistrano/fiftyfive/compatibility.rb
@@ -1,7 +1,8 @@
+require 'colorize'
+
 if Capistrano::VERSION == "3.2.0"
-  $stderr.puts(Term::ANSIColor.on_red {
+  $stderr.puts\
     "WARNING: Capistrano 3.2.0 has a critical bug that prevents "\
     "capistrano-fiftyfive from working as intended:\n"\
-    "https://github.com/capistrano/capistrano/issues/1004"
-  })
+    "https://github.com/capistrano/capistrano/issues/1004".colorize(:red)
 end
