fca1a445bc074e1bfbb35a12910759f943879338
Matt Brictson <matt@55minutes.com>
2014-05-31 14:10:14 -0700

    Show an error of gem is loaded incorrectly.
    Closes #2.

--- lib/capistrano/fiftyfive/compatibility.rb
+++ lib/capistrano/fiftyfive/compatibility.rb
@@ -1,5 +1,14 @@
 require 'colorize'
 
+unless defined?(Capistrano) && respond_to?(:namespace)
+  $stderr.puts\
+    "WARNING: capistrano/fiftyfive must be loaded by Capistrano in order "\
+    "to work.\nRequire this gem by using Capistrano's Capfile, "\
+    "as described here:\n"\
+    "https://github.com/55minutes/capistrano-fiftyfive#installation"\
+    .colorize(:red)
+end
+
 if Capistrano::VERSION == "3.2.0"
   $stderr.puts\
     "WARNING: Capistrano 3.2.0 has a critical bug that prevents "\
